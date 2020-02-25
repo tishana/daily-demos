bf494ef4e1310c089a6933be490963bdd4c6d6c4
Paul Kompfner <kompfner@gmail.com>
2020-02-25 18:56:36 -0500

    Remove configuration in `static-demos/index.js` that was only being used by demos that were removed in the previous commit (ones that didn't appear to be working). Also update serve-handler configuration to only serve files in the `static-demos` folder and to exclude files not relevant to the demos themselves (like package.json or images for the doc site).

--- static-demos/index.js
+++ static-demos/index.js
@@ -1,22 +1,15 @@
-const handler = require('serve-handler');
-require('dotenv').config()
-
-const config = {
-  DEMO_MEETING_URL: process.env.DEMO_MEETING_URL,
-  DEMO_MEETING_TOKEN: process.env.DEMO_MEETING_TOKEN,
-  MODERATED_DEMO_MEETING_URL: process.env.MODERATED_DEMO_MEETING_URL,
-  MODERATED_DEMO_MEETING_OWNER_TOKEN: process.env.MODERATED_DEMO_MEETING_OWNER_TOKEN,
-  MODERATED_DEMO_MEETING_MEMBER_TOKEN: process.env.MODERATED_DEMO_MEETING_MEMBER_TOKEN
-};
+const handler = require("serve-handler");
 
 module.exports = async (req, res) => {
   console.log(req.url);
-  if (req.url === '/env') {
-    res.end(JSON.stringify(config));
-    return;
-  }
   await handler(req, res, {
-    public: '..',
-    cleanUrls: false
+    public: "./",
+    cleanUrls: false,
+    unlisted: [
+      "index.js",
+      "package.json",
+      "package-lock.json",
+      "doc-site-assets"
+    ]
   });
-}
+};
