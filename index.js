14ec1e7d6de6aaf493f3d20cc7874887aeef00f7
Paul Kompfner <kompfner@gmail.com>
2020-02-28 11:22:48 -0500

    Remove cache control header from root page request, allowing CDN to cache however it sees fit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
+  res.sendFile("index.html", { root: __dirname, cacheControl: false });
 });
 
 app.use(
