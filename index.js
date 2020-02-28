fa98d32462e58768b63c4b29629ef04f9ecb1c27
Paul Kompfner <kompfner@gmail.com>
2020-02-27 19:10:07 -0500

    Use PORT environment variable where available

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
