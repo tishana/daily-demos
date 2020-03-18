f20ae5420b9bb929b23fc3dbaa2953feba37527d
Paul Kompfner <kompfner@gmail.com>
2020-03-18 11:47:34 -0400

    Rename App component to Call, and wrap it in a new higher-level App component, in preparation for the join flow

--- react-demo/src/App/App.js
+++ react-demo/src/App/App.js
@@ -1,51 +1,8 @@
-import React, { useEffect, useReducer } from "react";
-import DailyIframe from "@daily-co/daily-js";
-import "./App.css";
-import Participant from "../Participant/Participant";
-import { ADD_TRACK, REMOVE_TRACK, participantsReducer } from "./participants";
+import React from "react";
+import Call from "../Call/Call";
 
 function App(props) {
-  const [participants, dispatch] = useReducer(participantsReducer, {});
-
-  function trackStarted(e) {
-    dispatch({
-      type: ADD_TRACK,
-      sessionId: e.participant.session_id,
-      track: e.track,
-      isLocal: e.participant.local
-    });
-  }
-
-  function trackStopped(e) {
-    dispatch({ type: REMOVE_TRACK, track: e.track });
-  }
-
-  // Initialize the call object when the roomUrl is set (e.g. when the component mounts)
-  useEffect(() => {
-    const callObject = DailyIframe.createCallObject();
-    callObject.join({ url: props.roomUrl });
-    callObject.on("track-started", trackStarted);
-    callObject.on("track-stopped", trackStopped);
-  }, [props.roomUrl]);
-
-  // Render participant components
-  const participantCount = Object.keys(participants).length;
-  return (
-    <>
-      {Object.entries(participants).map(([sessionId, participant]) => {
-        return (
-          <Participant
-            key={sessionId}
-            sessionId={sessionId}
-            videoTrack={participant.video}
-            audioTrack={participant.audio}
-            isLocal={participant.isLocal}
-            totalParticipantCount={participantCount}
-          />
-        );
-      })}
-    </>
-  );
+  return <Call roomUrl="https://paulk.daily.co/hello" />;
 }
 
 export default App;
---
+++ react-demo/src/Call/Call.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useReducer } from "react";
+import DailyIframe from "@daily-co/daily-js";
+import "./Call.css";
+import Participant from "../Participant/Participant";
+import { ADD_TRACK, REMOVE_TRACK, participantsReducer } from "./participants";
+
+function Call(props) {
+  const [participants, dispatch] = useReducer(participantsReducer, {});
+
+  function trackStarted(e) {
+    dispatch({
+      type: ADD_TRACK,
+      sessionId: e.participant.session_id,
+      track: e.track,
+      isLocal: e.participant.local
+    });
+  }
+
+  function trackStopped(e) {
+    dispatch({ type: REMOVE_TRACK, track: e.track });
+  }
+
+  // Initialize the call object when the roomUrl is set (e.g. when the component mounts)
+  useEffect(() => {
+    const callObject = DailyIframe.createCallObject();
+    callObject.join({ url: props.roomUrl });
+    callObject.on("track-started", trackStarted);
+    callObject.on("track-stopped", trackStopped);
+  }, [props.roomUrl]);
+
+  // Render participant components
+  const participantCount = Object.keys(participants).length;
+  return (
+    <>
+      {Object.entries(participants).map(([sessionId, participant]) => {
+        return (
+          <Participant
+            key={sessionId}
+            sessionId={sessionId}
+            videoTrack={participant.video}
+            audioTrack={participant.audio}
+            isLocal={participant.isLocal}
+            totalParticipantCount={participantCount}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+export default Call;
