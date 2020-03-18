1202ca3f02f8edd14615eaf62c873ff48b3a338b
Paul Kompfner <kompfner@gmail.com>
2020-03-17 23:20:59 -0400

    Refactor App state management to use `useReducer` rather than `useState`, since state is complex enough

--- react-demo/src/App/App.js
+++ react-demo/src/App/App.js
@@ -1,90 +1,91 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import DailyIframe from "@daily-co/daily-js";
 import "./App.css";
 import Participant from "../Participant/Participant";
 
+// ADD_TRACK action structure:
+// - type: String
+// - track: MediaStreamTrack
+// - sessionId: String
+// - isLocal: Boolean
+const ADD_TRACK = "ADD_TRACK";
+
+// REMOVE_TRACK action structure:
+// - type
+// - track
+const REMOVE_TRACK = "REMOVE_TRACK";
+
+function removeProperty(prop, obj) {
+  const { [prop]: _, ...newObj } = obj;
+  return newObj;
+}
+
+// Participant state structure:
+// - audio: MediaStreamTrack
+// - video: MediaStreamTrack
+// - isLocal: Boolean
+function participantReducer(participant, action) {
+  switch (action.type) {
+    case ADD_TRACK:
+      return { ...participant, [action.track.kind]: action.track };
+    case REMOVE_TRACK:
+      return removeProperty(action.track.kind, participant);
+    default:
+      throw new Error();
+  }
+}
+
+// Participants state structure:
+// { [sessionId]: [participant state] }
+function participantsReducer(participants, action) {
+  // Returns [sessionId, participant] pair
+  function findByTrack(track) {
+    return Object.entries(participants).find(([_, p]) => {
+      return p[track.kind] && p[track.kind].id === track.id;
+    });
+  }
+
+  switch (action.type) {
+    case ADD_TRACK: {
+      const participant = participants[action.sessionId] || {
+        isLocal: action.isLocal
+      };
+      return {
+        ...participants,
+        [action.sessionId]: participantReducer(participant, action)
+      };
+    }
+    case REMOVE_TRACK: {
+      const [sessionId, participant] = findByTrack(action.track);
+      const updatedParticipant = participantReducer(participant, action);
+      if (!(updatedParticipant.audio || updatedParticipant.video)) {
+        return removeProperty(sessionId, participants);
+      }
+      return { ...participants, [sessionId]: updatedParticipant };
+    }
+    default:
+      throw new Error();
+  }
+}
+
 function App(props) {
-  // Participants state, mapping sessionIds to objects with these properties:
-  // - audioTrack: MediaStreamTrack
-  // - videoTrack: MediaStreamTrack
-  // - isLocal: Boolean
-  const [participants, setParticipants] = useState({});
-
-  // Update participant state when a track starts
-  function trackStarted(e) {
-    const sessionId = e.participant.session_id;
-    const isLocal = e.participant.local;
-
-    setParticipants(prevParticipants => {
-      // Add track to participant state
-      const prevParticipant = prevParticipants[sessionId] || {};
-      let updatedParticipant = { ...prevParticipant, isLocal: isLocal };
-      switch (e.track.kind) {
-        case "video":
-          updatedParticipant.videoTrack = e.track;
-          break;
-        case "audio":
-          updatedParticipant.audioTrack = e.track;
-          break;
-        default:
-          break;
-      }
-
-      // Update participant in overall participants state
-      let updatedParticipants = { ...prevParticipants };
-      updatedParticipants[sessionId] = updatedParticipant;
-      return updatedParticipants;
-    });
-  }
-
-  // Update participant state when a track ends
-  function trackStopped(e) {
-    const trackId = e.track.id;
-
-    setParticipants(prevParticipants => {
-      // Remove track from participant state
-      let sessionId, prevParticipant, updatedParticipant;
-      switch (e.track.kind) {
-        case "video":
-          [sessionId, prevParticipant] = Object.entries(prevParticipants).find(
-            ([s, p]) => {
-              return p.videoTrack && p.videoTrack.id === trackId;
-            }
-          );
-          updatedParticipant = { ...prevParticipant };
-          delete updatedParticipant.videoTrack;
-          break;
-        case "audio":
-          [sessionId, prevParticipant] = Object.entries(prevParticipants).find(
-            ([s, p]) => {
-              return p.audioTrack && p.audioTrack.id === trackId;
-            }
-          );
-          updatedParticipant = { ...prevParticipant };
-          delete updatedParticipant.audioTrack;
-          break;
-        default:
-          break;
-      }
-      console.log("updatedParticipant", updatedParticipant);
-
-      // Update or delete participant from overall participants state
-      let updatedParticipants = { ...prevParticipants };
-      if (!(updatedParticipant.audioTrack || updatedParticipant.videoTrack)) {
-        delete updatedParticipants[sessionId];
-      } else {
-        updatedParticipants[sessionId] = updatedParticipant;
-      }
-      return updatedParticipants;
-    });
-  }
+  const [participants, dispatch] = useReducer(participantsReducer, {});
 
   // Initialize the call object when the roomUrl is set (e.g. when the component mounts)
   useEffect(() => {
     const callObject = DailyIframe.createCallObject();
     callObject.join({ url: props.roomUrl });
-    callObject.on("track-started", trackStarted);
-    callObject.on("track-stopped", trackStopped);
+    callObject.on("track-started", e =>
+      dispatch({
+        type: ADD_TRACK,
+        sessionId: e.participant.session_id,
+        track: e.track,
+        isLocal: e.participant.local
+      })
+    );
+    callObject.on("track-stopped", e =>
+      dispatch({ type: REMOVE_TRACK, track: e.track })
+    );
   }, [props.roomUrl]);
 
   // Render participant components
@@ -96,8 +97,8 @@ function App(props) {
           <Participant
             key={sessionId}
             sessionId={sessionId}
-            videoTrack={participant.videoTrack}
-            audioTrack={participant.audioTrack}
+            videoTrack={participant.video}
+            audioTrack={participant.audio}
             isLocal={participant.isLocal}
             totalParticipantCount={participantCount}
           />
