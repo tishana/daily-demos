021324c5e5dcc89fd22df6cf09753b7c9f5da3e8
Paul Kompfner <kompfner@gmail.com>
2020-03-20 20:09:27 -0400

    Add "click allow" message if local user audio and video streams haven't started within some period of time

--- react-demo/src/Call/Call.js
+++ react-demo/src/Call/Call.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import "./Call.css";
 import Tile from "../Tile/Tile";
-import Invite from "../Invite/Invite";
+import Invite from "../CallMessage/CallMessage";
 import CallObjectContext from "../CallObjectContext";
+import CallMessage from "../CallMessage/CallMessage";
 
 // Call items here are representations of logical "inputs" into the call.
 // Each is represented visually as a tile, and may have accompanying audio.
@@ -63,16 +64,41 @@ function containsScreenShare(callItems) {
   return Object.keys(callItems).some(id => isScreenShare(id));
 }
 
+function localPerson(callItems) {
+  return callItems["local"];
+}
+
 // Props
 // - roomUrl: String
 function Call(props) {
   const callObject = useContext(CallObjectContext);
   const [callItems, setCallItems] = useState(initialCallItems);
+  const [showClickAllow, setShowClickAllow] = useState(false);
+
+  // Unfortunately the below two lines are a bit of weird complexity due to being a stateful functional component.
+  // We need access to the *latest* callItems when showClickAllowMessageIfNeeded() is called on a timeout.
+  // TODO: this is wonky. This will go away when we have a CallState object (then we can update using setState(() => {}))
+  const callItemsRef = useRef(callItems);
+  callItemsRef.current = callItems; // Updates references every render
+
+  function showClickAllowMessageIfNeeded() {
+    const callItems = callItemsRef.current;
+    const localPersonCallItem = localPerson(callItems);
+    if (!(localPersonCallItem && !localPersonCallItem.isLoading)) {
+      setShowClickAllow(true);
+    }
+  }
 
   function handleParticipantsChanged(e) {
     console.log("[daily.co event]", e.action);
     const participants = callObject.participants();
-    setCallItems(prevCallItems => getCallItems(participants, prevCallItems));
+    setCallItems(prevCallItems => {
+      const newCallItems = getCallItems(participants, prevCallItems);
+      if (localPerson(newCallItems) && !localPerson(newCallItems).isLoading) {
+        setShowClickAllow(false); // TODO: this is wonky. This will go away when we have a CallState object.
+      }
+      return newCallItems;
+    });
   }
 
   // Join the call when the roomUrl is set (e.g. when the component mounts).
@@ -82,6 +108,7 @@ function Call(props) {
       callObject.on("participant-updated", handleParticipantsChanged);
       callObject.on("participant-left", handleParticipantsChanged);
       callObject.join({ url: props.roomUrl });
+      setTimeout(showClickAllowMessageIfNeeded, 2500);
     }
   }, [props.roomUrl]);
 
@@ -109,11 +136,22 @@ function Call(props) {
     }
   });
 
+  let message = "";
+  if (showClickAllow) {
+    message = 'Click "Allow" to enable camera and mic access';
+  } else if (Object.keys(callItems).length === 1) {
+    message = "Copy and share this page's URL to invite others";
+  }
+
   // Render call
   return (
     <div className="call">
-      {Object.keys(callItems).length === 1 && <Invite />}
-      <div className="large-tiles">{largeTiles}</div>
+      {message && <CallMessage message={message} />}
+      <div className="large-tiles">
+        {!message
+          ? largeTiles
+          : null /* Avoid showing large tiles to make room for the message */}
+      </div>
       <div className="small-tiles">{smallTiles}</div>
     </div>
   );
---
+++ react-demo/src/CallMessage/CallMessage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CallMessage.css";
+
+// Props:
+// - message
+function CallMessage(props) {
+  return <p className="call-message">{props.message}</p>;
+}
+
+export default CallMessage;
