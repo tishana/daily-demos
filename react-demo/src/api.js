d0e19eda02e5f3ca23134df9407f6023c4e9db52
Paul Kompfner <kompfner@gmail.com>
2020-03-23 19:28:20 -0400

    Add comment to api wrapper to encourage sending "exp" parameter when creating a room

--- react-demo/src/api.js
+++ react-demo/src/api.js
@@ -3,7 +3,12 @@ const newRoomEndpoint =
 
 /**
  * Create a short-lived room for demo purposes.
- * To use the API to create your own rooms, see https://docs.daily.co/reference.
+ *
+ * IMPORTANT: when calling the real REST API, it's a good idea to pass an
+ * "exp" (expiration) parameter so you don't end up with a huge number of live
+ * rooms.
+ *
+ * See https://docs.daily.co/reference#create-room for details.
  */
 async function createRoom() {
   let response = await fetch(newRoomEndpoint),
